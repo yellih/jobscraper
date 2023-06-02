ac656efefbd1fdaed6af1a99cef9d5ad91c2ce78
yellih <yerihahn@gmail.com>
2023-06-02 18:44:27 +0900

    view all button

--- wwr.py
+++ wwr.py
@@ -1,14 +1,37 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-base_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
+wwr_url = "https://weworkremotely.com/"
+search_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
 search_term = "python"
+view_all_url = ""
 
-response = get(f"{base_url}{search_term}")
+search_url_response = get(f"{search_url}{search_term}")
 
 
-soup = BeautifulSoup(response.text, "html.parser")
+
+soup = BeautifulSoup(search_url_response.text, "html.parser")
 section = soup.find_all('section',class_="jobs")
+# print(section[-1])
 for ul in section:
-    view = ul.find_all('li' > 'a')
-    print(view[-1]['href'])
+    view = ul.find_all('li',class_='view-all')
+    # print(view)
+    for li in view:
+        link = li.find_all('a')
+        view_all_url = link[0]['href']
+        # print(view_all_url)
+        
+view_all_url_response = get(f"{wwr_url}{view_all_url}")
+# print(view_all_url_response)
+
+soup = BeautifulSoup(view_all_url_response.text, "html.parser")
+section = soup.find_all('section',class_="jobs")
+# print(section2)
+for ul in section:
+    li = ul.find_all('li')
+    # print(li)
+    # print(len(li))
+    li.pop(-1)
+    for post in li:
+        anchors = post.find_all('a')
+        # print(anchors)
