3fbf9e1040ec7eaf7303d70163973450ce3536d9
yellih <yerihahn@gmail.com>
2023-06-02 18:49:00 +0900

    status code and posts

--- wwr.py
+++ wwr.py
@@ -8,30 +8,42 @@ view_all_url = ""
 
 search_url_response = get(f"{search_url}{search_term}")
 
-
-
-soup = BeautifulSoup(search_url_response.text, "html.parser")
-section = soup.find_all('section',class_="jobs")
-# print(section[-1])
-for ul in section:
-    view = ul.find_all('li',class_='view-all')
-    # print(view)
-    for li in view:
-        link = li.find_all('a')
-        view_all_url = link[0]['href']
-        # print(view_all_url)
+if search_url_response.status_code != 200:
+    print("Can't request website")
+else:
+    soup = BeautifulSoup(search_url_response.text, "html.parser")
+    section = soup.find_all('section',class_="jobs")
+    # print(section[-1])
+    for ul in section:
+        view = ul.find_all('li',class_='view-all')
+        # print(view)
+        for li in view:
+            link = li.find_all('a')
+            view_all_url = link[0]['href']
+            # print(view_all_url)
         
 view_all_url_response = get(f"{wwr_url}{view_all_url}")
 # print(view_all_url_response)
 
-soup = BeautifulSoup(view_all_url_response.text, "html.parser")
-section = soup.find_all('section',class_="jobs")
-# print(section2)
-for ul in section:
-    li = ul.find_all('li')
-    # print(li)
-    # print(len(li))
-    li.pop(-1)
-    for post in li:
-        anchors = post.find_all('a')
-        # print(anchors)
+if view_all_url_response.status_code != 200:
+    print("Can't request website")
+else:
+    soup = BeautifulSoup(view_all_url_response.text, "html.parser")
+    section = soup.find_all('section',class_="jobs")
+    # print(section2)
+    for ul in section:
+        li = ul.find_all('li')
+        # print(li)
+        # print(len(li))
+        li.pop(-1)
+        for post in li:
+            anchors = post.find_all('a')
+            # print(anchors)
+            anchor = anchors[1]
+            link = anchor['href']
+            # print(anchor)
+            company, kind, region = anchor.find_all('span', class_="company")
+            # print(company,kind, region)
+            title = anchor.find('span', class_='title')
+            print(company,kind, region, title)
+            print('//')
