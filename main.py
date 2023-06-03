de8aeb2e325dd37b1ffc7d917d5b5ec0c827771a
yellih <yerihahn@gmail.com>
2023-06-03 13:26:08 +0900

    status code 확인

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service  #브라우저 실행 후 자동 닫힘 방지1
 from webdriver_manager.chrome import ChromeDriverManager  #브라우저 실행 후 자동 닫힘 방지2
+from requests import get
+from bs4 import BeautifulSoup
 
 options = Options()
 options.add_experimental_option('detach', True)
@@ -10,6 +12,11 @@ options.add_experimental_option('excludeSwitches',['enable-longging'])
 service = Service(ChromeDriverManager().install())
 browser = webdriver.Chrome(service=service,options=options)
 
-browser.get("https://kr.indeed.com/jobs?q=python")
+search_url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
 
-print(browser.page_source)
+if get(f"{search_url}{search_term}").status_code == 200:
+    print("Request Success!")
+else:
+    browser.get(f"{search_url}{search_term}")
+    print(browser.page_source)
