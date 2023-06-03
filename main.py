441644538dc58837effffb1cd54d46b77358c9eb
yellih <yerihahn@gmail.com>
2023-06-03 13:07:44 +0900

    selenium 테스트

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from requests import get
-from bs4 import BeautifulSoup
-from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service  #브라우저 실행 후 자동 닫힘 방지1
+from webdriver_manager.chrome import ChromeDriverManager  #브라우저 실행 후 자동 닫힘 방지2
 
-jobs = extract_wwr_jobs("python")
-print(jobs)
+options = Options()
+options.add_experimental_option('detach', True)
+options.add_experimental_option('excludeSwitches',['enable-longging'])
+
+service = Service(ChromeDriverManager().install())
+browser = webdriver.Chrome(service=service,options=options)
+
+browser.get("https://kr.indeed.com/jobs?q=python")
