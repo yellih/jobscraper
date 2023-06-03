bb118ccba919a0542538dd4959f4cd55086d9d16
yellih <yerihahn@gmail.com>
2023-06-03 13:07:03 +0900

    변수 이름 변경

--- extractors/wwr.py
+++ extractors/wwr.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 
 def extract_wwr_jobs(keyword):
 
-    wwr_url = "https://weworkremotely.com"
+    home_url = "https://weworkremotely.com"
     search_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
-    view_all_url = ""
+    page_url = ""
     search_url_response = get(f"{search_url}{keyword}")
 
     if search_url_response.status_code != 200:
@@ -19,18 +19,18 @@ def extract_wwr_jobs(keyword):
             # print(view)
             for li in view:
                 link = li.find_all('a')
-                view_all_url = link[0]['href']
-                # print(view_all_url)
+                page_url = link[0]['href']
+                # print(page_url)
             
-    view_all_url_response = get(f"{wwr_url}{view_all_url}")
-    # print(f"{wwr_url}{view_all_url}")
-    # print(view_all_url_response)
+    page_url_response = get(f"{home_url}{page_url}")
+    # print(f"{home_url}{page_url}")
+    # print(page_url_response)
 
-    if view_all_url_response.status_code != 200:
+    if page_url_response.status_code != 200:
         print("Can't request website")
     else:
         results = []
-        soup = BeautifulSoup(view_all_url_response.text, "html.parser")
+        soup = BeautifulSoup(page_url_response.text, "html.parser")
         section = soup.find_all('section',class_="jobs")
         # print(section)
         for ul in section:
@@ -51,7 +51,7 @@ def extract_wwr_jobs(keyword):
                 # print(company,kind, region)
                 title = anchor.find('span', class_='title')
                 job_data = {
-                    'link':wwr_url+link,
+                    'link':home_url+link,
                     'company': company.string,
                     'region': region.string,
                     'position': title.string
