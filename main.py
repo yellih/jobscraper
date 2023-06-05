32e87f787459972290fdae6b1dfc577826cdeb3a
yellih <yerihahn@gamil.com>
2023-06-05 18:33:16 +0900

    result of indeed and wwr

---
+++ main.py
@@ -0,0 +1,15 @@
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+
+keyword = input("What do you want to search for?")
+
+indeed = extract_indeed_jobs(keyword)
+wwr = extract_wwr_jobs(keyword)
+
+jobs = indeed + wwr
+
+# print(jobs)
+
+for job in jobs:
+    print(job)
+    print("//\n//")
