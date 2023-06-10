33b03b4d25dc102baf257680e86b7356ed223d5e
yellih <yerihahn@gamil.com>
2023-06-10 22:27:24 +0900

    한글 깨짐 문제로 인한 Internal server error 500 해결

--- file.py
+++ file.py
@@ -1,5 +1,5 @@
 def save_to_file(file_name, jobs):
-    file = open(f"{file_name}.csv", "w")
+    file = open(f"{file_name}.csv", "w" ,encoding='UTF-8')
     file.write("Position,Company,Location,URL\n")
 
     for job in jobs:
