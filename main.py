e71c5bb8cd185453410dcdee0da6c1e301a83b9f
yellih <yerihahn@gamil.com>
2023-06-10 19:05:17 +0900

    deliver jobs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
 
 app = Flask("JobScrapper")
 
@@ -14,6 +16,9 @@ def hello():
 def search():
     # print(request.args)
     keyword = request.args.get("keyword")
-    return render_template("search.html", keyword=keyword )
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = indeed + wwr
+    return render_template("search.html", keyword=keyword, jobs = jobs )
 
 app.run("0.0.0.0")
